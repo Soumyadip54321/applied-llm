fa59746c9f93311e660b7417eb09cb6b81cc7ede
soumyadip <soumyadiperikson@gmail.com>
2026-02-08 22:49:50 +0530

    Modified code to capture transcribed audio by assemblyAI and if fails to have whisper transcribe it

--- NewsResearchTool/backend/speech_to_text.py
+++ NewsResearchTool/backend/speech_to_text.py
@@ -1,9 +1,10 @@
 '''
-Script that performs STT(Speech To Text) using OPENAI's Whisper model.
+Script that performs STT(Speech To Text) using ASSEMBLYAI model.
 This is used to convert audio recording of user's query to question which then is fed to the LLM model along with URLs to fetch
 token response.
 '''
 import os
+import time
 from functools import lru_cache
 import whisper
 import tempfile
@@ -13,28 +14,17 @@ from langchain.agents import create_agent
 
 load_dotenv('.env')
 
-# fetch assembly ai API key
-assemblyai_api_key = os.getenv('ASSEMBLYAI_API_KEY')
-
-# fetch openai API key
+# fetch assemblyAI & OPENAI API key
+aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
 openai_api_key = os.getenv('OPENAI_API_KEY')
-
-# set model to rectify transcription error such that the user query is framed correctly.
-model = ChatOpenAI(
-    model='gpt-5.2',
-    api_key=openai_api_key,
-    temperature=0.0, # higher number indicates more randomness in the model's output
-    max_tokens=500, #defines the no of words in the model's response
-    timeout=30 # max time in sec to wait for model's response
-)
 
 @lru_cache
 def load_whisper_model():
     '''
-    Function that loads Whisper model from OpenAI's Whisper model and caches it in memory.
+    Function that loads 'base' WHISPER model from disk to be used as a failsafe option in case AssemblyAI fails to transcribe audio.
     :return:
     '''
-    return whisper.load_model('medium')
+    return whisper.load_model('base')
 
 def save_audio_file(audio):
     '''
@@ -50,57 +40,36 @@ def save_audio_file(audio):
         # output file path to .wav
         return f.name
 
-def fix_transcription_error(transcribed_audio : str)->str:
-    '''
-    Function that fixes transcription error in STT model.
-    :return:
-    '''
-    system_prompt = f"""
-                You are correcting speech-to-text errors.
-                
-                Rules:
-                - Do NOT add new information
-                - Do NOT change the meaning
-                - Only fix obvious transcription mistakes
-                - Preserve the original intent
-                - Preserve question form if present
-                
-                Original transcription:
-                "{transcribed_audio}"
-                
-                Return ONLY the corrected sentence.
-                """
-
-    agent = create_agent(
-        model=model,
-        tools=[],
-        system_prompt=system_prompt
-    )
-
-    corrected_transcription = agent.invoke({"messages":[{"role":"user","content":transcribed_audio}]})
-    return corrected_transcription.content.strip()
-
-
 def transcribe_audio(audio)->str:
     '''
-    Function that converts audio file to text using OpenAI's Whisper model before removing the audio file stored in disc.
-    :return: dictionary that contains text converted from audio.
+    Function that uses AssemblyAI model which if fails uses Whisper model to convert audio containing user question to text.
+    :return: text captured from audio.
     '''
     # get audio file path
     audio_file_path = save_audio_file(audio)
 
-    #whisper by default assumes availability of GPU and converts audio bytes to floating point 16 numbers. In case of CPU
-    # this causes issues.
-    # result = whisper_model.transcribe(audio_file_path,fp16=False,language='en',
-    #         task='transcribe',temperature=0.0,initial_prompt = "Indian English accent. News, economy, elections, sports etc.")
+    # transcribe audio to text using universal-2 model
+    config = aai.TranscriptionConfig(
+        speech_models=["universal-2"]  # or "universal-3-pro"
+    )
 
-    # transcribe audio to text
-    result = aai.Transcriber().transcribe(audio_file_path)
+    # Assembly AI creates a job to transcribe the audio file with status=queued.
+    result = aai.Transcriber(config=config).transcribe(audio_file_path)
+
+    # since transcription is asynchronous in nature we wait for sometime before fetching the job to check whether it got completed or errored out
+    while result.status not in ["completed", "error"]:
+        # wait 1 sec prior to fetching job
+        time.sleep(1)
+        # fetch updated job which's assumed to contain the converted text from audio & status
+        result = transcriber.get_transcript(result.id)
+
+    # if job errored out use whisper model to transcribe audio.
+    if result.status == "error":
+        whisper_model = load_whisper_model()
+        result = whisper_model.transcribe(audio_file_path,language="en")
 
     # clean-up the temp audio file stored in disc
     os.remove(audio_file_path)
 
-    # fix erroneous transcription
-    # corrected_transcription = fix_transcription_error(result.text.strip())
     return result.text.strip()
 
